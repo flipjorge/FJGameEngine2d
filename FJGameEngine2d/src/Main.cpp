b7b7ebf3cbccae1fe73a3c0b774aa4b2122ab2bf
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2022-07-19 23:19:19 +0100

    Initialize window and renderer

--- FJGameEngine2d/src/Main.cpp
+++ FJGameEngine2d/src/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <SDL.h>
 
-int main(int argc, char* argv[])
+typedef struct
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+} App;
+
+void initSDL(App& app)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
@@ -9,6 +15,28 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    std::cout << "Hello, world!" << std::endl;
+    app.window = SDL_CreateWindow("FJGameEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+
+    if (!app.window)
+    {
+        SDL_Log("Unable to create window: %s", SDL_GetError());
+        exit(1);
+    }
+
+    app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!app.renderer)
+    {
+        SDL_Log("Unable to create renderer: %s", SDL_GetError());
+        exit(1);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    App app;
+    
+    initSDL(app);
+
     return 0;
 }
