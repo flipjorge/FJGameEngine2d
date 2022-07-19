e4cd74cfe62b8ce9663ab7d8928fd9827fd6df27
Filipe Jorge <filipe.pereira.jorge@gmail.com>
2022-07-19 22:05:09 +0100

    Check if SDL inits properly

--- FJGameEngine2d/src/Main.cpp
+++ FJGameEngine2d/src/Main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <SDL.h>
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        exit(1);
+    }
+
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
